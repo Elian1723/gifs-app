5078d30fc9fe8ceed431a86b19cb449bad85e7ab
Elian1723 <elianbarrios1707@outlook.com>
2026-02-25 15:51:34 -0600

    Implement search functionality and update search page layout

--- src/app/gifs/pages/search-page/search-page.ts
+++ src/app/gifs/pages/search-page/search-page.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { GifList } from "../../components/gif-list/gif-list";
+import { GifsApi } from '../../services/gifsApi';
+import { Gif } from '../../interfaces/gif';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifList],
   templateUrl: './search-page.html',
 })
-export default class SearchPage { }
+export default class SearchPage {
+  protected gifApi = inject(GifsApi);
+  protected gifs = signal<Gif[]>([]);
+
+  protected onSearch(query: string){
+    this.gifApi.searchGifs(query).subscribe(resp => {
+      this.gifs.set(resp);
+    });
+  }
+}
--- src/app/gifs/services/gifsApi.ts
+++ src/app/gifs/services/gifsApi.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment.development';
 import { type GiphyResponse } from '../interfaces/giphy';
 import { Gif } from '../interfaces/gif';
 import { GifMapper } from '../mapper/gif-mapper';
+import { map, Observable } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class GifsApi {
@@ -15,16 +16,29 @@ export class GifsApi {
     this.loadTrendingGifs();
   }
 
-  public loadTrendingGifs() {
+  public loadTrendingGifs(): void {
     this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
       params: {
         api_key: environment.giphyApiKey,
         limit: 20
       }
-    }).subscribe((resp) => {
+    }).subscribe(resp => {
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
       this.trendingGifs.set(gifs);
       this.trendingGifsLoading.set(false);
     });
+  }
+
+  public searchGifs(query: string): Observable<Gif[]> {
+    return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+      params: {
+        api_key: environment.giphyApiKey,
+        q: query,
+        limit: 20
+      }
+    }).pipe(
+      map(({data}) => data),
+      map(giphyItems => GifMapper.mapGiphyItemsToGifArray(giphyItems))
+    )
   }
 }
